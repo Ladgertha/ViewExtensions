6ec20c4bb06d1777e244d632b88ee9de6bdb63e1
Anna <myandroidblog@yandex.ru>
2021-05-02 18:53:57 +0300

    Library is done

---
+++ view_extensions/src/main/java/ru/ladgertha/view_extensions/ViewExtensions.kt
@@ -0,0 +1,15 @@
+package ru.ladgertha.view_extensions
+
+import android.view.View
+
+fun View.hide() {
+    this.visibility = View.GONE
+}
+
+fun View.show() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.invisible() {
+    this.visibility = View.INVISIBLE
+}
